d465eed125d3373f80b86ec046e8cf9c4b54e25d
SonuKumar <sonu.shukla@syncrasytech.com>
2025-02-13 11:55:03 +0530

    update

--- .history/resources/js/app_20250213112652.js
+++
@@ -1,6 +0,0 @@
-import './bootstrap';
-Echo.channel('talk-proposals')
-    .listen('TalkProposalSubmitted', (e) => {
-        console.log('New talk proposal submitted:', e.talkProposal);
-        // Update the UI accordingly, e.g., append to a list
-    });
--- .history/resources/views/talk_proposal/create.blade_20250212163352.php
+++
@@ -1,9 +0,0 @@
-<!-- resources/views/talk_proposal/create.blade.php -->
-<form action="{{ route('talk_proposals.store') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <input type="text" name="title" placeholder="Title" required>
-    <textarea name="description" placeholder="Description" required></textarea>
-    <input type="text" name="tags" placeholder="Tags (comma-separated)" required>
-    <input type="file" name="presentation" accept=".pdf" required>
-    <button type="submit">Submit Proposal</button>
-</form>
